5f91ca1b63fea56ddf0acb04eed239f1a1460fcd
nkc20236093 <nkc23.taki.eita@gmail.com>
2023-08-04 15:24:29 +0900

    ジャンプを実装

--- fighting3D/fighting3d/Assets/FightingMotionsVolume1/charactor/character_Controller/otoko_chara1/otoko_chara_Controller.cs
+++ fighting3D/fighting3d/Assets/FightingMotionsVolume1/charactor/character_Controller/otoko_chara1/otoko_chara_Controller.cs
@@ -4,26 +4,34 @@ using UnityEngine;
 
 public class otoko_chara_Controller : MonoBehaviour
 {
+    //Rayを宣言
+    Ray ray;
+    //レイを飛ばす距離
+    float distance = 10f;
+    //レイが何かに当たった時の情報
+    RaycastHit hit;
+    //レイを発射する位置
+    Vector3 rayPos;
+
     //移動の変数
     float sayuu;
     float jouge;
 
     //ジャンプパワー（統一予定）
-    ForceMode jump = (ForceMode)1000;
+    float jump_power = 15f;
 
+    //CharacterControllerを宣言
+    public CharacterController characterController;
 
     //左右用の移動方向変数
     Vector3 sayuu_houkou = Vector3.zero;
 
-    //RigidBodyを変数に保存
-    Rigidbody rigidbody;
-
     //ジャンプの速度を設定
-    const float _velocity = 5.0f;
+    float Jump_velocity = 5.0f;
 
     //着地状態を管理
-    public bool _isGrounded;  //着地してない=false
-                              //着地してる  =true
+    public bool jump_isGrounded;  //着地してない=false
+                              　　//着地してる  =true
 
     //各初期ステータス
 
@@ -42,14 +50,42 @@ public class otoko_chara_Controller : MonoBehaviour
     void Start()
     {
         Application.targetFrameRate = 60;
-        this.rigidbody = GetComponent < Rigidbody > ();
         //最初は着地してない状態
-        //_isGrounded = false;
+        jump_isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //レイを発射する位置の調整
+        rayPos = transform.position + new Vector3(0, 0.5f, 0);
+        //レイを下に飛ばす
+        ray = new Ray(rayPos, transform.up * -1);
+        //デバッグ用のレイを発光
+        Debug.DrawRay(ray.origin, ray.direction * distance, Color.red);
+
+        //ジャンプしてない間、重力をかける
+        Jump_velocity += Physics.gravity.y * Time.deltaTime;
+        Debug.Log("重力");
+
+        //着地状態か確認
+        if (Physics.Raycast(ray, out hit, distance))
+        {
+            //接地判定を確実にするため-0.5fを代入
+            Jump_velocity = -0.5f;
+            Debug.Log("着地状態");
+            //レイが地面にヒットしてるか確認
+            if (hit.collider.tag == "jimen")
+            {
+                jump_isGrounded = true;
+                Debug.Log("jimen");
+            }
+            else
+            {
+                jump_isGrounded = false;
+                Debug.Log("hazure");
+            }
+        }
 
         //変数にHorizontal・Verticalを代入
         sayuu = Input.GetAxisRaw("Horizontal");
@@ -84,41 +120,32 @@ public class otoko_chara_Controller : MonoBehaviour
 
         //ジャンプ(スティック or 上矢印キー(Wキー))
 
-        //着地状態か確認
-        if (_isGrounded == true)
+        //地面についてたら
+        if (jump_isGrounded == true)
         {
-            Debug.Log("着地状態");
             //ジャンプ(スティック or 上矢印キー(Wキー))が押されてるか確認
             if (jouge > 0)
             {
                 Debug.Log("ジャンプ");
+                //地面から離れるので着地状態を書き換え
+                jump_isGrounded = false;
+                //Y軸の速度を代入
+                Jump_velocity = jump_power;
+
                 //ジャンプの方向を上向きのベクトルに設定
-                Vector3 jump_vector = Vector3.up;
+                //Vector3 jump_vector = Vector3.up;
 
                 //ジャンプの速度を計算
-                Vector3 jump_velocity = jump_vector * _velocity;
+                //Vector3 jump_velocity = jump_vector * Jump_velocity;
 
                 //上向きの速度を設定
-                rigidbody.velocity = jump_velocity;
+                //rigidbody.velocity = jump_velocity;
 
-                rigidbody.AddForce(rigidbody.velocity,jump);
+                //rigidbody.AddForce(rigidbody.velocity, jump_power);
 
-                //地面から離れるので着地状態を書き換え
-                _isGrounded = false;
             }
+            characterController.Move(new Vector3(0, Jump_velocity, 0) * Time.deltaTime);
         }
+        
     }
-    void OnCollisionEnter(Collision collision)
-    {
-        {
-            if (collision.gameObject.tag == "jimen")
-            {
-                Debug.Log("着地成功");
-                _isGrounded = true;
-            }
-        }
-    }
-
-    
-
 }
