3600ada4c69f56f4fb9367a8ec14806851586076
nkc20236093 <nkc23.taki.eita@gmail.com>
2023-08-15 20:05:22 +0900

    characterControllerを使用した当たり判定の制作に難航中

--- fighting3D/fighting3d/fighting3d/Assets/dekoi.cs
+++ fighting3D/fighting3d/fighting3d/Assets/dekoi.cs
@@ -2,11 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Otoko_chara_Controller : MonoBehaviour
+public class dekoi : MonoBehaviour
 {
-    //攻撃を受けた・与えた状態を管理する用の変数
-    public float kougeki_attack;
-
     //Transformコンポーネントを取得
     Transform mytransform;
     //向き変更用変数
@@ -31,11 +28,11 @@ public class Otoko_chara_Controller : MonoBehaviour
     float dash_speed;
     //現在のスピードモード
     float now_speed;
- 
+
     //Rayを宣言
     Ray ray;
     //レイを飛ばす距離
-    float distance = 0.001f;
+    public float distance = 0.001f;
     //レイが何かに当たった時の情報
     RaycastHit hit;
     //レイを発射する位置
@@ -128,107 +125,105 @@ public class Otoko_chara_Controller : MonoBehaviour
                 jump_isGrounded = false;
             }
         }
-        //以下基本動作
+        ////以下基本動作
 
-        //弱攻撃（X or J）
-        if (Input.GetAxisRaw("X or J") != 0)
-        {
-            animator.SetInteger("stop", 2);
-            Debug.Log("弱攻撃");
-            kougeki_attack = 1;
-        }
-        //各行動終了次第停止状態に変更
-        else
-        {
-            //アニメーション変更
-            Invoke(nameof(animation_stop), 5f);
-        }
-        //強攻撃（A or K）
-        if (Input.GetAxisRaw("A or K") != 0)
-        {
-            Debug.Log("強攻撃");
-        }
-        //投げ攻撃（B or L）
-        if (Input.GetAxisRaw("B or L") != 0)
-        {
-            Debug.Log("投げ攻撃");
-        }
-        //必殺技（Y or I）
-        if (Input.GetAxisRaw("Y or I") != 0)
-        {
-            Debug.Log("必殺技");
-        }
-        //ガード(Right(left) Bumper or sperce)   ※ジャストガードも検討
-        if (Input.GetButtonDown("Right(left) Bumper or sperce")) 
-        {
-            Debug.Log("ガード");
-        }
-        //変数にHorizontal・Verticalを代入
-        PlayerVector = new Vector3(0, jouge, sayuu * 0.15f * chara_muki);
-        sayuu = Input.GetAxisRaw("Horizontal");
-        jouge = Input.GetAxisRaw("Vertical");
+        ////弱攻撃（X or J）
+        //if (Input.GetAxisRaw("X or J") != 0)
+        //{
+        //    animator.SetInteger("stop", 2);
+        //    Debug.Log("弱攻撃");
+        //}
+        ////各行動終了次第停止状態に変更
+        //else
+        //{
+        //    //アニメーション変更
+        //    animation_stop();
+        //}
+        ////強攻撃（A or K）
+        //if (Input.GetAxisRaw("A or K") != 0)
+        //{
+        //    Debug.Log("強攻撃");
+        //}
+        ////投げ攻撃（B or L）
+        //if (Input.GetAxisRaw("B or L") != 0)
+        //{
+        //    Debug.Log("投げ攻撃");
+        //}
+        ////必殺技（Y or I）
+        //if (Input.GetAxisRaw("Y or I") != 0)
+        //{
+        //    Debug.Log("必殺技");
+        //}
+        ////ガード(Right(left) Bumper or sperce)   ※ジャストガードも検討
+        //if (Input.GetButtonDown("Right(left) Bumper or sperce"))
+        //{
+        //    Debug.Log("ガード");
+        //}
+        ////変数にHorizontal・Verticalを代入
+        //PlayerVector = new Vector3(0, jouge, sayuu * 0.15f * chara_muki);
+        //sayuu = Input.GetAxisRaw("Horizontal");
+        //jouge = Input.GetAxisRaw("Vertical");
 
-        //向き変更用
-        if (sayuu > 0)
-        {
-            muki = false;
-        }
-        else if (sayuu < 0)
-        {
-            muki = true;
-        }
-        //地面についてたらジャンプ力を0にする
-        if (jump_isGrounded == true || rayhit == true) 
-        {
-            jouge = 0f;
-        }
-        //横移動(スティック or 左右矢印キー)&ジャンプ(スティック or 上矢印キー(Wキー))    
-        transform.Translate(PlayerVector);
+        ////向き変更用
+        //if (sayuu > 0)
+        //{
+        //    muki = false;
+        //}
+        //else if (sayuu < 0)
+        //{
+        //    muki = true;
+        //}
+        ////地面についてたらジャンプ力を0にする
+        //if (jump_isGrounded == true || rayhit == true)
+        //{
+        //    jouge = 0f;
+        //}
+        ////横移動(スティック or 左右矢印キー)&ジャンプ(スティック or 上矢印キー(Wキー))    
+        //transform.Translate(PlayerVector);
 
-        //以下アニメーション
+        ////以下アニメーション
 
-        //ワールド座標を基準に回転を取得
-        Vector3 World_angle = mytransform.eulerAngles;
-        //左右どちらかに移動中
-        if (sayuu != 0)
-        {
-            //右移動
-            if (muki == false)
-            {
-                //反転処理
-                World_angle.y = -90;
-                chara_muki = 1;
-                //アニメーション変更
-                animator.SetInteger("stop", 1);
-            }
-            //左移動
-            else
-            {
-                //反転処理
-                World_angle.y = 90;
-                chara_muki = -1;
-                //アニメーション変更
-                animator.SetInteger("stop", 1);
-            }
-        }
-        //停止状態
-        else
-        {
-            //アニメーション変更
-            Invoke(nameof(animation_stop), 1f);
-        }
-        mytransform.eulerAngles = World_angle;
+        ////ワールド座標を基準に回転を取得
+        //Vector3 World_angle = mytransform.eulerAngles;
+        ////左右どちらかに移動中
+        //if (sayuu != 0)
+        //{
+        //    //右移動
+        //    if (muki == false)
+        //    {
+        //        //反転処理
+        //        World_angle.y = -90;
+        //        chara_muki = 1;
+        //        //アニメーション変更
+        //        animator.SetInteger("stop", 1);
+        //    }
+        //    //左移動
+        //    else
+        //    {
+        //        //反転処理
+        //        World_angle.y = 90;
+        //        chara_muki = -1;
+        //        //アニメーション変更
+        //        animator.SetInteger("stop", 1);
+        //    }
+        //}
+        ////停止状態
+        //else
+        //{
+        //    //アニメーション変更
+        //    Invoke(nameof(animation_stop), 1f);
+        //}
+        //mytransform.eulerAngles = World_angle;
     }
     //停止状態のアニメーション
     void animation_stop()
     {
         animator.SetInteger("stop", 0);
-        kougeki_attack = 0;
     }
     //characterControllerを利用した当たり判定
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (hit.gameObject.CompareTag("jimen"))  
+        if (hit.gameObject.CompareTag("jimen"))
         {
             Debug.Log("jimen");
             jump_isGrounded = true;
@@ -236,16 +231,8 @@ public class Otoko_chara_Controller : MonoBehaviour
         }
         if (hit.gameObject.CompareTag("Player"))
         {
-            Debug.Log("確認");
-        }
-        ChakItem(hit.collider.gameObject);
-    }
-    void ChakItem(GameObject obj)
-    {
-        if (obj.CompareTag("Player") && kougeki_attack > 0)
-        {
-            Debug.Log("hit_player");
-            Invoke(nameof(animation_stop), 5f);
+            Debug.Log("hit_dekoi");
+
         }
     }
 }
