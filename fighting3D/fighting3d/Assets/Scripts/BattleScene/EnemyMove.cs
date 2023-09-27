f570eb40c9e702475fc9c5fe2c8946adc4639c79
nkc20236176 <nkc23.denpa.sugiura.masaya@gmail.com>
2023-09-27 17:14:02 +0900

    koimmtto

---
+++ fighting3D/fighting3d/Assets/Scripts/BattleScene/EnemyMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour
+{
+
+
+
+
+    enum State
+    {
+        jab,
+        kick,
+        Hidari,
+        Migi
+
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
